ec9c49576c154370ee56b45034ec10408e361981
Vince Carini <vincecarini@northwesternmutual.com>
2018-02-01 08:07:13 -0600

    remove warning

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import './ui-toolkit/css/nm-cx/main.css';
-import { connect } from 'react-redux';
 import DossierDisplay from './DossierDisplay';
 import TabInputSection from './TabInputSection';
 
